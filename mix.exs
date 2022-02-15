a820db13f47a4257b0c07e9b1d1b5fafccd0907e
Rafael Epplée <hello@rafa.ee>
2022-02-15 21:24:46 +0100

    add dialyzer

--- mix.exs
+++ mix.exs
@@ -10,7 +10,8 @@ defmodule Minin.MixProject do
       compilers: [:gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      dialyzer: [plt_add_deps: :transitive]
     ]
   end
 
@@ -48,7 +49,8 @@ defmodule Minin.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 0.5.0", only: [:dev], runtime: false}
     ]
   end
 
