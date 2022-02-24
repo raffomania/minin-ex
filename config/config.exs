49c4a9e6eb4dd6b08df61664960b6f964cdaea62
Rafael Epplée <hello@rafa.ee>
2022-02-24 17:24:23 +0100

    configure ci db via env vars

--- config/config.exs
+++ config/config.exs
@@ -35,6 +35,10 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+config :minin, Minin.Repo,
+  username: System.get_env("DB_USER"),
+  password: System.get_env("DB_PASSWORD")
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
